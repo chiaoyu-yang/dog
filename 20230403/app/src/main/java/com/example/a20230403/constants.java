f744727dfe62edeca16534706f4426e9c658d3ca
chiaoyu <10856042@ntub.edu.tw>
2023-05-24 16:46:45 +0800

    汪星問

---
+++ 20230403/app/src/main/java/com/example/a20230403/constants.java
@@ -0,0 +1,7 @@
+package com.example.a20230403;
+
+public class constants {
+    private static final String ROOT_URL = "http://140.131.114.145/Android/v1/";
+
+    public static final String URL_QUESTION = ROOT_URL + "randomQuestion.php";
+}
