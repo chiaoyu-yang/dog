c6615fecd6738d14168d992b17879bf09b69b759
yehch624 <83951517+yehch624@users.noreply.github.com>
2023-05-26 01:50:43 +0800

    Add files via upload

---
+++ 後端/Android/myRank.php
@@ -0,0 +1,30 @@
+<?php 
+header('Content-Type: text/html; charset=utf-8');
+require_once '../includes/DbOperations.php';
+
+$response = array(); 
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    // if (isset($_POST['email'])){
+        //operate the data further 
+
+    $db = new DbOperations(); 
+
+    $result = $db->getMyranks();
+
+    if($result){
+            $response['myranks'] = $result;
+    }else{
+            $response['message'] = "Failed to retrieve ranks.";
+    }
+    // }else{
+    //     $response['error'] = true;
+    //     $response['message'] = "Email parameter is missing.";
+    // }
+}else{
+    $response['message'] = "Invalid request method.";
+}
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
+
+?>
