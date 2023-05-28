8d41eb140066741161e633345b166cd4c2614b96
chiaoyu <10856042@ntub.edu.tw>
2023-05-29 02:12:17 +0800

    login+home+QA

--- soulgo/app/src/main/java/com/example/soulgo/Constants.java
+++ soulgo/app/src/main/java/com/example/soulgo/Constants.java
@@ -4,6 +4,4 @@ public class Constants {
     private static final String ROOT_URL = "http://140.131.114.145/Android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
-
-    public static final String URL_QUESTION = ROOT_URL + "randomQuestion.php";
 }
