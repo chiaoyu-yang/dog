7cc0e410c711b4879d98d35af7f7a94d7c2d3a7d
chihyulin <10856035@ntub.edu.tw>
2023-05-29 21:45:48 +0800

    setting

---
+++ setting/app/src/main/java/com/example/setting/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.setting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton settingIcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        settingIcon = findViewById(R.id.settingIcon);
+        settingIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openaSetting();
+            }
+        });
+    }
+
+    public void openaSetting() {
+        Intent intent = new Intent(this,SettingActivity.class);
+        startActivity(intent);
+    }
+
+}
