aec224d637153032db8ca8001cfda51293e1813b
volley-910 <83951549+volley-910@users.noreply.github.com>
2023-06-01 22:32:16 +0800

    Delete ConSetting directory

--- ConSetting/app/src/main/java/com/example/consetting/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.consetting;
-
-public class Constants {
-    private static final String ROOT_URL = "http://140.131.114.145/Android/includes/";
-
-    public static final String URL_Setting = ROOT_URL + "EditName.php";
-}
-
