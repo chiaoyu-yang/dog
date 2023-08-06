f114c1e1f63acf8722fedb6918f646bcbda5a741
chiaoyu <10856042@ntub.edu.tw>
2023-08-06 10:26:38 +0800

    tidy
    tidy_0806

--- AndroidStudioProjects/soulgo/app/src/main/java/com/example/soulgo/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.soulgo;
-
-public class Constants {
-    private static final String ROOT_URL = "http://140.131.114.145/Android/v1/";
-
-    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
-}
