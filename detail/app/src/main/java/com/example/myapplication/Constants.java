604ef10e8657d420953308655e07289300e0562a
hsiao <10856003@ntub.edu.tw>
2023-09-14 23:35:39 +0800

    詳細資訊 - 完成

--- detail/app/src/main/java/com/example/myapplication/Constants.java
+++ detail/app/src/main/java/com/example/myapplication/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     private static final String ROOT_URL = "http://140.131.114.145/Android/v1/";
 
     public static final String URL_DETAIL = ROOT_URL + "detail.php";
+    public static final String URL_UPDATE_DETAIL = ROOT_URL + "updateDetail.php";
+    public static final String URL_GET_DOG_IMAGE = ROOT_URL + "updateDetail.php";
 }
